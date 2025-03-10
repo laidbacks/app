769bcbd016c71bf8250cf1342e0fe8f3df478274
Javokhir <ganiboevjavokhir@gmail.com>
2025-03-10 14:00:54 -0400

    Log In Feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :require_login, only: [:show]
+
   def new
     @user = User.new
     render :signup
@@ -21,18 +23,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    # More explicit error handling and debugging
-    if session[:user_id].present?
-      @user = User.find_by(id: session[:user_id])
-      if @user.nil?
-        # This indicates the user ID in the session is invalid
-        Rails.logger.error("Failed to find user with ID #{session[:user_id]}")
-        redirect_to signup_path, alert: "User not found. Please sign up or log in."
-      end
-    else
-      # No user ID in the session
-      redirect_to signup_path, alert: "Please sign up or log in first."
-    end
+    @user = current_user
   end
 
   private
@@ -40,11 +31,6 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:username, :password, :password_confirmation)
   end
-
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-  end
-  helper_method :current_user # Makes this method available in views
 
   def generate_token(user)
     # Using JWT for token generation
