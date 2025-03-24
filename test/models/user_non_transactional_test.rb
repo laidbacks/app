dea337b7753383c60e5fed6dfdcedb1e1d5dbe2a
Behruz Pulatov <behruzpulatov2014@gmail.com>
2025-03-24 14:08:50 -0400

    Commit: Rubocop linting implemented

--- test/models/user_non_transactional_test.rb
+++ test/models/user_non_transactional_test.rb
@@ -3,70 +3,70 @@ require "test_helper"
 class UserNonTransactionalTest < ActiveSupport::TestCase
   # Disable transactional tests for this test case
   self.use_transactional_tests = false
-  
+
   # Setup - runs before each test
   setup do
     # Since we're not using transactions, we need to manually clean up data
     # that could be left from previous tests
-    User.where(username: ["non_transactional_user", "another_non_transactional_user"]).destroy_all
+    User.where(username: [ "non_transactional_user", "another_non_transactional_user" ]).destroy_all
   end
-  
+
   # Teardown - runs after each test
   teardown do
     # Clean up any data we created in tests
-    User.where(username: ["non_transactional_user", "another_non_transactional_user"]).destroy_all
+    User.where(username: [ "non_transactional_user", "another_non_transactional_user" ]).destroy_all
   end
-  
+
   # This test creates a record that will persist to the database
   # (not rolled back automatically)
   test "creates a persistent record" do
     initial_count = User.count
-    
+
     # Create a user that will not be rolled back
     User.create!(username: "non_transactional_user", password: "password123")
-    
+
     # Verify the user was created
     assert_equal initial_count + 1, User.count
     assert User.exists?(username: "non_transactional_user")
   end
-  
+
   # This test will see the user created in the previous test if run sequentially
   # which demonstrates the lack of transaction isolation
   test "can see records created in other tests" do
     # Create another test user
     another_user = User.create!(username: "another_non_transactional_user", password: "password123")
-    
+
     # Count users with our test usernames
     test_users_count = User.where(
-      username: ["non_transactional_user", "another_non_transactional_user"]
+      username: [ "non_transactional_user", "another_non_transactional_user" ]
     ).count
-    
+
     # If the previous test ran, we should have both users
     # If tests run in isolation or in parallel, we might only have the one we just created
     assert_operator test_users_count, :>=, 1
     assert User.exists?(id: another_user.id)
   end
-  
+
   # This test demonstrates why you might want to disable transactions
   # for example, when testing threaded code or background jobs
   test "can test behavior that spans processes" do
     # Create a user
     User.create!(username: "non_transactional_user", password: "password123")
-    
+
     # In a real scenario, you might run a background job here that would
     # operate on this user. Since it's in a separate process, it needs
     # to see the committed data.
-    
+
     # Simulate running a job in a separate process with system call
     # system("bin/rails runner 'puts User.find_by(username: \"non_transactional_user\").inspect'")
-    
+
     # Or simulate it with a direct query to verify the user exists in the database
     # not just in the current connection's transaction
     ActiveRecord::Base.establish_connection
     assert_equal 1, ActiveRecord::Base.connection.select_value(
       "SELECT COUNT(*) FROM users WHERE username = 'non_transactional_user'"
     ).to_i
-    
+
     # No need to reconnect as we're just using the same connection
   end
-end 
+end
--- test/models/user_transaction_test.rb
+++ test/models/user_transaction_test.rb
@@ -2,33 +2,33 @@ require "test_helper"
 
 class UserTransactionTest < ActiveSupport::TestCase
   # Tests demonstrating transaction behavior in Rails tests
-  
+
   # By default, all tests are wrapped in a transaction that is rolled back
   test "changes in one test don't affect other tests" do
     initial_count = User.count
-    
+
     # Create a new user that should be rolled back after the test
     User.create!(username: "transaction_test_user", password: "password123")
-    
+
     # Verify the user was created in this test
     assert_equal initial_count + 1, User.count
     assert User.exists?(username: "transaction_test_user")
   end
-  
+
   # This test should not see the user created in the previous test
   # because that transaction was rolled back
   test "previous test changes are not visible" do
     # The user from the previous test should not exist
     assert_not User.exists?(username: "transaction_test_user")
   end
-  
+
   # Test that current_transaction works as expected even with the
   # implicit transaction around tests
   test "current_transaction method works correctly" do
     # The implicit transaction around tests doesn't interfere with
     # application-level semantics of current_transaction
     assert_not_nil User.connection.current_transaction
-    
+
     # Start a new transaction
     User.transaction do
       # Now we should have a transaction
@@ -37,7 +37,7 @@ class UserTransactionTest < ActiveSupport::TestCase
       assert User.connection.transaction_open?
     end
   end
-  
+
   # Test nested transactions with savepoints
   test "nested transactions work correctly" do
     # In Rails, nested transactions are implemented with savepoints
@@ -45,20 +45,20 @@ class UserTransactionTest < ActiveSupport::TestCase
     initial_count = User.count
     outer_username = "outer_transaction_#{Time.now.to_i}"
     inner_username = "inner_transaction_#{Time.now.to_i}"
-    
+
     # Start an outer transaction
     User.transaction do
       # Create a user in the outer transaction
       outer_user = User.create!(username: outer_username, password: "password123")
       assert User.exists?(username: outer_username)
-      
+
       # Start a nested transaction with a savepoint
       begin
         User.transaction(requires_new: true) do
           # Create a user in the inner transaction
           inner_user = User.create!(username: inner_username, password: "password123")
           assert User.exists?(username: inner_username)
-          
+
           # Roll back the inner transaction only
           raise ActiveRecord::Rollback
         end
@@ -66,20 +66,20 @@ class UserTransactionTest < ActiveSupport::TestCase
         # Should not reach here
         flunk "Inner transaction rollback should not propagate"
       end
-      
+
       # The inner transaction should have been rolled back
       # But in Rails, the parent transaction is not affected by child rollbacks
       assert User.exists?(username: outer_username)
       # This is key: in Rails with savepoints, the inner transaction changes *should* be rolled back
       assert_not User.exists?(username: inner_username)
-      
+
       # Roll back the outer transaction
       raise ActiveRecord::Rollback
     end
-    
+
     # After the outer transaction, all changes should be rolled back
     assert_equal initial_count, User.count
     assert_not User.exists?(username: outer_username)
     assert_not User.exists?(username: inner_username)
   end
-end 
+end
