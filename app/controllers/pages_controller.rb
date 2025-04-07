10a2a561bd1dc54bd0207857d5c9f4d08ec7d5aa
Samiuddin AHmed <samiuddinahmed@usf.edu>
2025-04-07 17:39:43 -0400

    Commit:

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,12 +13,16 @@ class PagesController < ApplicationController
 
       # Quote of the day
       @quotes = [
-        "The secret of getting ahead is getting started. – Mark Twain",
-        "Small habits make a big difference. – James Clear",
-        "We are what we repeatedly do. Excellence, then, is not an act, but a habit. – Aristotle",
-        "Success is the sum of small efforts, repeated day in and day out. – Robert Collier",
-        "Your habits become your values, your values become your destiny. – Mahatma Gandhi",
-        "Champions keep playing until they get it right. – Billie Jean King"
+        "Most people overestimate what they can do in one year and underestimate what they can do in ten. – Bill Gates",
+        "Move fast and break things. Unless you are breaking stuff, you are not moving fast enough. – Mark Zuckerberg",
+        "There is nothing impossible to him who will try. – Alexander the Great",
+        "Persistence is very important. You should not give up unless you are forced to give up. – Elon Musk",
+        "I'm convinced that about half of what separates the successful entrepreneurs from the non-successful ones is pure perseverance. – Steve Jobs",
+        "You earn reputation by trying to do hard things well. – Jeff Bezos",
+        "Genius is one percent inspiration and ninety-nine percent perspiration. – Thomas Edison",
+        "The future rewards those who press on. I don't have time to feel sorry for myself. I don't have time to complain. I'm going to press on. – Barack Obama",
+        "The man who moves a mountain begins by carrying away small stones. – Confucius",
+        "I fear not the man who has practiced 10,000 kicks once, but I fear the man who has practiced one kick 10,000 times. – Bruce Lee"
       ]
       @quote = @quotes.sample
 
