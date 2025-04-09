e8306c317e814e5560ac463ab3c975eeb44de06e
Samiuddin AHmed <samiuddinahmed@usf.edu>
2025-04-09 16:58:48 -0400

    Commit:

--- script/tomain.sh
+++ script/tomain.sh
@@ -1,6 +1,6 @@
 bundle exec rubocop -a
 git add .
-git commit -m "Frontend Progress tracking"
+git commit -m "Updated UI for greeting message and made it more interactive"
 git push -u origin
 git checkout main
 git pull
